dfc2158521a34f20b9ac527ce39c348ad033dafd
nicokestel <nicokestel@gmail.com>
2017-05-16 18:09:42 +0200

    Add files via upload

--- sprYt/core/src/de/inf16_17/spryt/SprytMain.java
+++ sprYt/core/src/de/inf16_17/spryt/SprytMain.java
@@ -1,33 +1,49 @@
 package de.inf16_17.spryt;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 
-public class SprytMain extends ApplicationAdapter {
+public class SprytMain extends Game {
+	
 	SpriteBatch batch;
-	Texture img;
+	Sprite img;
+	
+	float rot;
 	
 	@Override
 	public void create () {
+		
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Sprite(new Texture("badlogic.jpg"));
+		img.setOrigin(img.getWidth() / 2, img.getHeight() / 2);
+		img.setPosition(Gdx.graphics.getWidth() / 2 - img.getWidth() / 2, Gdx.graphics.getHeight() / 2 - img.getHeight() / 2);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 1, 0, 1);
+		
+		Gdx.gl.glClearColor(1 - img.getRotation() / 30, 1, img.getRotation() / 30, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		img.draw(batch);
 		batch.end();
+		
+		float degreesPerSecond = 10.0f;
+		rot = (rot + Gdx.graphics.getDeltaTime() * degreesPerSecond) % 360;
+		
+		float shakeAmplitudeInDegrees = 30.0f;
+		float shake = MathUtils.sin(rot) * shakeAmplitudeInDegrees;
+		
+		img.setRotation(shake);
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
