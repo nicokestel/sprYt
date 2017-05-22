0ef78ce9be132ed761b352ad98fd1a7325ae8395
nicokestel <nicokestel@gmail.com>
2017-05-22 14:23:10 +0200

    Add files via upload

--- sprYt/core/src/de/inf16_17/spryt/SprytMain.java
+++ sprYt/core/src/de/inf16_17/spryt/SprytMain.java
@@ -9,59 +9,65 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 
+import de.inf16_17.spryt.screens.GameScreen;
+
 public class SprytMain extends Game {
 	
-	SpriteBatch batch;
-	Sprite img;
-	float rot;
-	Color color;
+//	SpriteBatch batch;
+//	Sprite img;
+//	float rot;
+//	Color color;
 	
 	@Override
 	public void create () {
 		
-		batch = new SpriteBatch();
-		img = new Sprite(new Texture("badlogic.jpg"));
-		img.setOrigin(img.getWidth() / 2, img.getHeight() / 2);
-		img.setPosition(Gdx.graphics.getWidth() / 2 - img.getWidth() / 2, Gdx.graphics.getHeight() / 2 - img.getHeight() / 2);
-		color = new Color(1, 1, 1, 1);
+		this.setScreen(new GameScreen(this));
+		
+//		batch = new SpriteBatch();
+//		img = new Sprite(new Texture("badlogic.jpg"));
+//		img.setOrigin(img.getWidth() / 2, img.getHeight() / 2);
+//		img.setPosition(Gdx.graphics.getWidth() / 2 - img.getWidth() / 2, Gdx.graphics.getHeight() / 2 - img.getHeight() / 2);
+//		color = new Color(1, 1, 1, 1);
 	}
 
 	@Override
 	public void render () {
-		
-		update(Gdx.graphics.getDeltaTime());
-		
-		Gdx.gl.glClearColor(color.r, color.g, color.b, color.a);
+		Gdx.gl.glClearColor(0.5f, 0.1f, 0.8f, 1f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		img.draw(batch);
-		batch.end();
+		
+		super.render();
+		
+//		update(Gdx.graphics.getDeltaTime());
+//		
+//		Gdx.gl.glClearColor(color.r, color.g, color.b, color.a);
+//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//		batch.begin();
+//		img.draw(batch);
+//		batch.end();
 	}
 	
-	private void update(float deltaTime) {
-		
-		if (Gdx.input.justTouched()) {
-			img.setPosition(Gdx.input.getX() - img.getWidth() / 2, -Gdx.input.getY() + Gdx.graphics.getHeight() -  img.getHeight() / 2);
-			System.out.println(Gdx.input.getX() + " : " + Gdx.input.getY());
-			color.r = MathUtils.random();
-			color.g = MathUtils.random();
-			color.b = MathUtils.random();
-			color.a = MathUtils.random();
-		}else if (Gdx.input.isTouched()) {
-			img.setPosition(Gdx.input.getX() - img.getWidth() / 2, -Gdx.input.getY() + Gdx.graphics.getHeight() -  img.getHeight() / 2);
-			float degreesPerSecond = 50.0f;
-			rot = (rot + Gdx.graphics.getDeltaTime() * degreesPerSecond) % 360;
-			float shakeAmplitudeInDegrees = 10.0f;
-			float shake = MathUtils.sin(rot) * shakeAmplitudeInDegrees;
-			img.setRotation(shake);
-			Gdx.input.vibrate(10);
-		}else if (!Gdx.input.isTouched(0)) {
-			img.setRotation(0);
-		}
-	}
+//	private void update(float deltaTime) {
+//		
+//		if (Gdx.input.justTouched()) {
+//			img.setPosition(Gdx.input.getX() - img.getWidth() / 2, -Gdx.input.getY() + Gdx.graphics.getHeight() -  img.getHeight() / 2);
+//			System.out.println(Gdx.input.getX() + " : " + Gdx.input.getY());
+//			color.r = MathUtils.random();
+//			color.g = MathUtils.random();
+//			color.b = MathUtils.random();
+//			color.a = MathUtils.random();
+//		}else if (Gdx.input.isTouched()) {
+//			img.setPosition(Gdx.input.getX() - img.getWidth() / 2, -Gdx.input.getY() + Gdx.graphics.getHeight() -  img.getHeight() / 2);
+//			float degreesPerSecond = 50.0f;
+//			rot = (rot + Gdx.graphics.getDeltaTime() * degreesPerSecond) % 360;
+//			float shakeAmplitudeInDegrees = 10.0f;
+//			float shake = MathUtils.sin(rot) * shakeAmplitudeInDegrees;
+//			img.setRotation(shake);
+//			Gdx.input.vibrate(10);
+//		}
+//	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
+//		batch.dispose();
 	}
 }
