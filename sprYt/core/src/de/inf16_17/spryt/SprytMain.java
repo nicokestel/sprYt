5566ea7f6bf3aa6e51695bd6fdf31fc5ff0e10d8
nicokestel <nicokestel@gmail.com>
2017-05-23 17:50:11 +0200

    Add files via upload

--- sprYt/core/src/de/inf16_17/spryt/SprytMain.java
+++ sprYt/core/src/de/inf16_17/spryt/SprytMain.java
@@ -1,73 +1,43 @@
 package de.inf16_17.spryt;
 
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
+import java.util.ArrayList;
 
-import de.inf16_17.spryt.screens.GameScreen;
+import com.badlogic.gdx.Game;
 
 public class SprytMain extends Game {
 	
-//	SpriteBatch batch;
-//	Sprite img;
-//	float rot;
-//	Color color;
+	private ArrayList<Player> players;
 	
 	@Override
 	public void create () {
-		
-		this.setScreen(new GameScreen(this));
-		
-//		batch = new SpriteBatch();
-//		img = new Sprite(new Texture("badlogic.jpg"));
-//		img.setOrigin(img.getWidth() / 2, img.getHeight() / 2);
-//		img.setPosition(Gdx.graphics.getWidth() / 2 - img.getWidth() / 2, Gdx.graphics.getHeight() / 2 - img.getHeight() / 2);
-//		color = new Color(1, 1, 1, 1);
+//		this.setScreen(new GameScreen(this));
+		init();
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0.5f, 0.1f, 0.8f, 1f);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	private void init() {
 		
-		super.render();
+		// ArrayList initialisieren
+		players = new ArrayList<Player>();
+		// Spieler hinzufuegen
+		players.add(0, new Player("Nico Kestel", Gender.MALE, Sex.HETERO, true));
+		players.add(1, new Player("Malik Geiger", Gender.MALE, Sex.HETERO, true));
+		players.add(2, new Player("Matthias Müller", Gender.MALE, Sex.HETERO, false));
 		
-//		update(Gdx.graphics.getDeltaTime());
-//		
-//		Gdx.gl.glClearColor(color.r, color.g, color.b, color.a);
-//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-//		batch.begin();
-//		img.draw(batch);
-//		batch.end();
+		// Alle Spieler der Reihe nach ausgeben
+		for (int i = 0; i < players.size(); i++) {
+			Player player = players.get(i);
+			System.out.println(player.getName() + " " + player.getGender() + " " + player.getSex() + " " + player.isInRelationship());
+		}
 	}
 	
-//	private void update(float deltaTime) {
-//		
-//		if (Gdx.input.justTouched()) {
-//			img.setPosition(Gdx.input.getX() - img.getWidth() / 2, -Gdx.input.getY() + Gdx.graphics.getHeight() -  img.getHeight() / 2);
-//			System.out.println(Gdx.input.getX() + " : " + Gdx.input.getY());
-//			color.r = MathUtils.random();
-//			color.g = MathUtils.random();
-//			color.b = MathUtils.random();
-//			color.a = MathUtils.random();
-//		}else if (Gdx.input.isTouched()) {
-//			img.setPosition(Gdx.input.getX() - img.getWidth() / 2, -Gdx.input.getY() + Gdx.graphics.getHeight() -  img.getHeight() / 2);
-//			float degreesPerSecond = 50.0f;
-//			rot = (rot + Gdx.graphics.getDeltaTime() * degreesPerSecond) % 360;
-//			float shakeAmplitudeInDegrees = 10.0f;
-//			float shake = MathUtils.sin(rot) * shakeAmplitudeInDegrees;
-//			img.setRotation(shake);
-//			Gdx.input.vibrate(10);
-//		}
-//	}
+	@Override
+	public void render () {
+		
+		super.render();
+	}
 	
 	@Override
 	public void dispose () {
-//		batch.dispose();
+
 	}
 }
--- sprYt/core/src/de/inf16_17/spryt/screens/GameScreen.java
+++ sprYt/core/src/de/inf16_17/spryt/screens/GameScreen.java
@@ -1,25 +1,50 @@
 package de.inf16_17.spryt.screens;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 public class GameScreen implements Screen {
 	
 	private final Game game;
 	
-	private Stage stage;
+	private Skin skin;
 	
+	private Stage stage;
+	private TextButton btn;
 	
 	public GameScreen(final Game game){
 		this.game = game;
 		
+		skin = new Skin();
+		skin.addRegions(new TextureAtlas("assets/images-ui/uiskin.atlas"));
 		
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
+		
+		btn = new TextButton("Ich bin ein TextButton!", skin);
+		stage.addActor(btn);
+		btn.addListener(new EventListener() {
+			
+			@Override
+			public boolean handle(Event event) {
+				Gdx.app.debug("", "CLICKED");
+				return false;
+			}
+		});
 	}
 	
 	
 	@Override
 	public void dispose() {
+		
 		stage.dispose();
 	}
 
@@ -36,6 +61,11 @@ public class GameScreen implements Screen {
 	@Override
 	public void render(float arg0) {
 		
+		Gdx.gl.glClearColor(0.5f, 0.1f, 0.8f, 1f);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		stage.act();
+		stage.draw();
 	}
 
 	@Override
