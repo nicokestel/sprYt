db667f5336804d30750655be5e06490fd7b362f6
nicokestel <nicokestel@gmail.com>
2017-05-30 18:29:22 +0200

    Add files via upload

--- sprYt/core/src/de/inf16_17/spryt/TaskReader.java
+++ sprYt/core/src/de/inf16_17/spryt/TaskReader.java
@@ -1,63 +1,74 @@
 package de.inf16_17.spryt;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 
 public class TaskReader {
 	
-	// Liest die Aufgaben aus einer Datei ein
-	public static List<Task> readTasks(String path){
-		List<Task> tasks = new ArrayList<Task>();
-		
+	// Loeschen!!!!
+	public static void main(String[] args) {
+		ArrayList<Task> tasks = readTasks("src/testTask.txt");
+		for (Task task : tasks)
+			task.print();
+	}
+	
+	/**
+	 * Liest alle Aufgaben aus einer TXT-Datei ein
+	 * @param path Pfad zur TXT-Datei
+	 * @return Liste mit allen eingelesenen Aufgaben
+	 */
+	public static ArrayList<Task> readTasks(String path){
+		ArrayList<Task> tasks = new ArrayList<Task>();
 		try {
-			Scanner s = new Scanner(new File(path));
-			
-			while(s.hasNext()){
-				String line = s.nextLine();
+			Scanner fileScanner = new Scanner(new File(path)); // Liest die Datei Zeile fuer Zeile ein
+			while(fileScanner.hasNext()){
+				String line = fileScanner.nextLine();
 				
-				Scanner scanner = new Scanner(line);
-				StringReader reader = new StringReader(line);
+				StringReader stringReader = new StringReader(line); // Liest Aufgabe und Message ein
+				Scanner parameterScanner = new Scanner(line); // Liest Parameter ein
 				
 				int startIndex = 1, endIndex = 1;
-				reader.read();
-				while(reader.read() != '"'){
+				stringReader.read();
+				while(stringReader.read() != '"'){
 					endIndex++;
 				}
+				// Aufgabe
 				String taskText = line.substring(startIndex, endIndex);
 				
 				startIndex = endIndex + 3;
 				endIndex = startIndex;
-				reader.read();
-				reader.read();
-				while(reader.read() != '"'){
+				stringReader.read();
+				stringReader.read();
+				while(stringReader.read() != '"'){
 					endIndex++;
 				}
+				// Message
 				String msg = line.substring(startIndex, endIndex);
 				
-				scanner.skip("\"" + taskText + "\" \"" + msg + "\"");
+				// Aufgabe und Message ueberspringen; Springen zu Parameter
+				parameterScanner.skip("\"" + taskText + "\" \"" + msg + "\"");
 				
-				int consumption = scanner.nextInt();
-				int physicalContact = scanner.nextInt();
-				int activity = scanner.nextInt();
-				int game = scanner.nextInt();
-				int validity = scanner.nextInt();
-				boolean protection = scanner.nextInt() == 1;
+				// Parameter
+				int consumption = parameterScanner.nextInt();
+				int physicalContact = parameterScanner.nextInt();
+				int activity = parameterScanner.nextInt();
+				int game = parameterScanner.nextInt();
+				int validity = parameterScanner.nextInt();
+				boolean protection = parameterScanner.nextInt() == 1;
 				
 				Task task = new Task(taskText, msg, consumption, physicalContact, activity, game, validity, protection);
 				tasks.add(task);
 				
-				scanner.close();
-				reader.close();
+				parameterScanner.close();
+				stringReader.close();
 			}
 			
-			s.close();
+			fileScanner.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
