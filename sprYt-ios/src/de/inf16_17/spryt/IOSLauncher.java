0084b441aa2fb26063b7a329292202abfebdf66a
nicokestel <nicokestel@gmail.com>
2017-06-26 06:37:51 +0200

    Add files via upload

---
+++ sprYt-ios/src/de/inf16_17/spryt/IOSLauncher.java
@@ -0,0 +1,22 @@
+package de.inf16_17.spryt;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import de.inf16_17.spryt.SprytMain;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new SprytMain(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
